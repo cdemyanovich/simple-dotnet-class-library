03d9be4729cac4dc78bcb461e44e24b0ffbc1686
Craig Demyanovich <craig@8thlight.com>
2017-01-17 10:40:57 -0600

    Create HelloWorld and tests.

---
+++ SimpleClassLibrary/SimpleClassLibrary/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleClassLibrary
+{
+    public class HelloWorld
+    {
+        public string Greet()
+        {
+            return "Hello, world!";
+        }
+    }
+}
---
+++ SimpleClassLibrary/SimpleClassLibraryTests/HelloWorldTest.cs
@@ -0,0 +1,14 @@
+﻿using SimpleClassLibrary;
+using Xunit;
+
+namespace SimpleClassLibraryTests
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void Greets()
+        {
+            Assert.Equal("Hello, world!", new HelloWorld().Greet());
+        }
+    }
+}
